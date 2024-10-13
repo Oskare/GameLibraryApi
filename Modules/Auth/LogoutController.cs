f5f257f4c0a9b8319bd16462ac9511ba01bdce7e
Oskar Eriksson <oskare90@gmail.com>
2024-10-13 21:08:40 +0200

    Added logout

---
+++ Modules/Auth/LogoutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameLibraryApi.Modules.Auth;
+
+[ApiController]
+[Authorize]
+public class LogoutController(SignInManager<IdentityUser> signInManager)
+{
+    [HttpPost("auth/logout")]
+    public async Task Logout()
+    {
+        await signInManager.SignOutAsync();
+    }
+}
